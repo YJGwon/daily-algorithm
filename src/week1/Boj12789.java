7201797415ec87eef17199120650ca7ffd4799c5
YJGwon <trulyforky@gmail.com>
2023-02-04 16:09:38 +0900

    feat: solved boj11866

---
+++ src/week1/Boj12789.java
@@ -0,0 +1,44 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Boj12789 {
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+        final Stack<Integer> waitingLine = new Stack<>();
+
+        int order = 1;
+        for (int i = 0; i < N; i++) {
+            while (!waitingLine.isEmpty() && waitingLine.peek() == order) {
+                waitingLine.pop();
+                order++;
+            }
+            final int ticket = Integer.parseInt(st.nextToken());
+            if (ticket == order) {
+                order++;
+                continue;
+            }
+            waitingLine.push(ticket);
+        }
+
+        while (!waitingLine.isEmpty() && waitingLine.peek() == order) {
+            waitingLine.pop();
+            order++;
+        }
+
+        if (waitingLine.isEmpty()) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Sad");
+        }
+    }
+}
