af93398319ea4c5743ff0bc2b24d39862ff857f9
YJGwon <trulyforky@gmail.com>
2023-02-21 17:52:49 +0900

    refactor: enhanced boj14938

--- src/week4/Boj14938.java
+++ src/week4/Boj14938.java
@@ -6,10 +6,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -19,6 +20,7 @@ public class Boj14938 {
     static Map<Integer, List<int[]>> graph;
     static int[] items;
     static boolean[] visited;
+    static int[] minWeight;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,8 +53,10 @@ public class Boj14938 {
         }
 
         int maxItems = 0;
+        minWeight = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             visited = new boolean[n + 1];
+            Arrays.fill(minWeight, Integer.MAX_VALUE);
             maxItems = Math.max(maxItems, bfs(i));
         }
 
@@ -61,18 +65,19 @@ public class Boj14938 {
     }
 
     private static int bfs(final int region) {
-        final Queue<int[]> queue = new LinkedList<>();
+        final Queue<int[]> queue = new PriorityQueue<>((a, b) -> Arrays.compare(a, 1, 2, b, 1, 2));
         queue.offer(new int[]{region, 0});
         int itemCount = 0;
         while (!queue.isEmpty()) {
             final int[] current = queue.poll();
-            if (current[1] > m) {
+            if (current[1] > m || current[1] > minWeight[current[0]]) {
                 continue;
             }
             if (!visited[current[0]]) {
                 itemCount += items[current[0]];
             }
             visited[current[0]] = true;
+            minWeight[current[0]] = current[1];
             for (int[] next : graph.get(current[0])) {
                 queue.offer(new int[]{next[0], current[1] + next[1]});
             }
